7b3ad1b8ee506cb00feafc488932c2501d651472
Tommaso-Crocetti <t.crocetti1@studenti.unipi.it>
2024-10-31 00:04:12 +0100

    Backpropagation implementation

--- test.py
+++ test.py
@@ -4,8 +4,6 @@ import numpy as np
 import matplotlib
 import pandas as pd
 from ucimlrepo import fetch_ucirepo
-  
-
 
 # Definire un'interfaccia chiamata "Figura"
 class Function(ABC):
@@ -20,7 +18,6 @@ class Function(ABC):
 
 
 class Id(Function):
-
 
     def activation(self):
         def id(x):
@@ -78,10 +75,10 @@ class Layer:
         self.activation_derivate = activation_class.derivate()
         if layer_type == Type.INPUT:
             self.weights = weights
-            self.matrix = np.eye(neurons)
+            self.weight_matrix = np.eye(neurons)
         else:
             self.weights = weights + 1
-            self.matrix = np.random.random((self.neurons, self.weights))
+            self.weight_matrix = np.random.random((self.neurons, self.weights))
 
     def net(self, o):
         if self.type != Type.INPUT:
@@ -101,29 +98,50 @@ class Network:
 
     def __init__(self, hidden_layers_number, input_dimension, layer_length, activation_class_arr):
         self.depth = hidden_layers_number
+        self.store_hidden_result = [] 
         self.input_layer = Layer(input_dimension, input_dimension, Id(), Type.INPUT)
         self.hidden_layers = np.empty(self.depth, dtype=object)
         self.hidden_layers[0] = Layer(layer_length[0], input_dimension, activation_class_arr[0], Type.HIDDEN)
+        self.store_hidden_result.append(np.zeros(self.hidden_layers[0].neurons))
         for i in range(hidden_layers_number - 1):
             self.hidden_layers[i + 1] = Layer(layer_length[i + 1], self.hidden_layers[i].neurons, activation_class_arr[i + 1], Type.HIDDEN)
+            self.store_hidden_result.append(np.zeros(self.hidden_layers[i + 1].neurons))
         self.output_layer = Layer(layer_length[hidden_layers_number], self.hidden_layers[hidden_layers_number - 1].neurons, activation_class_arr[hidden_layers_number], Type.OUTPUT)
 
     def network_output(self, input):
         current_input = self.input_layer.act(input)
-        for i in range(self.depth):
-            current_input = self.hidden_layers[i].act(current_input)
-        return self.output_layer.act(current_input)
+        self.store_hidden_result[0] = self.hidden_layers[i].act(current_input)
+        for i in range(self.depth - 1):
+            self.store_hidden_result[i + 1] = self.hidden_layers[i].act(self.store_hidden_result[i])
+        return self.output_layer.act(self.store_hidden_result[self.depth - 1])
     
-    #nota: tiene conto della somma degli output dell'output layer
+    #nota: LMS e backprop. tengono conto di target value monodimensionali, quindi supponiamo di avere un solo neurone di output
     def LMS(self, X, y):
         error = 0
         for index, row in X.iterrows():
-            output_vec = self.network_output(row)
-            output = sum(output_vec)
+            output = self.network_output(row)
             target_value = y.iloc[index]
             error += np.square(target_value - output)
         return error
 
+    def backpropagation(self, X, y):
+        for index, row in X.iterrows():
+            output = self.network_output(row);
+            store_gradient = []
+            store_output_delta = (y.iloc[index] - output) * self.output_layer.der_act(self.store_hidden_result[self.depth - 1])
+            for i in range(self.output_layer.neurons):
+                store_gradient.append([])
+                for j in range(self.output_layer.weights):
+                    store_gradient[0].append(store_output_delta * self.store_hidden_result[self.depth - 1][j])
+            # array di array, contiene i delta dei neuroni degli hidden layer
+            store_hidden_layers_delta = []
+            current_hidden_layer = self.hidden_layers[self.depth - 1]
+            current_hidden_layer_delta = np.zeros(current_hidden_layer.neurons)
+            for index_neuron in range(current_hidden_layer.neurons):
+                current_hidden_layer_delta[index_neuron] = store_output_delta * self.output_layer.weight_matrix[0][index_neuron] * current_hidden_layer.der_act(self.store_hidden_result[self.depth - 2])
+                store_gradient.append([])
+                for j in range(self.current_hidden_layer.weights):
+                    store_gradient[1].append(current_hidden_layer_delta[index_neuron] * self.store_hidden_result[self.depth - 2][j])
 
     
 def main():
